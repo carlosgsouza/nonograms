21f802503820c70a0a2df09fe5b6b22d238f6959
Carlos Souza <carlosgsouza@gmail.com>
2021-12-05 17:49:09 -0800

    Adding a GridPrinter. Currently only grids of size <= 9 are supported.

---
+++ src/test/kotlin/carlosgsouza/nonograms/GridPrinterTest.kt
@@ -0,0 +1,45 @@
+package carlosgsouza.nonograms
+
+import carlosgsouza.nonograms.carlosgsouza.nonograms.Cell
+import carlosgsouza.nonograms.carlosgsouza.nonograms.GridPrinter
+import com.natpryce.hamkrest.allElements
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.jupiter.api.Test
+
+internal class GridPrinterTest {
+    private val factory = GridFactory()
+    private val printer = GridPrinter()
+
+    @Test
+    fun testPrint() {
+        val grid: Grid = factory.create("""
+SIZE
+3
+ROWS
+1 1
+2
+1
+COLUMNS
+1 1
+1
+2
+VALUES
+x _ x
+_ x x
+x _ ?
+""")
+
+        assertThat(printer.toString(grid), equalTo("""
+                  1    
+                  1 1 2
+                ┌───────┐
+            1 1 │ ■   ■ │
+              2 │   ■ ■ │
+              1 │ ■   ? │
+                └───────┘
+                """.trimIndent()))
+
+
+    }
+}
---
+++ src/test/kotlin/carlosgsouza/nonograms/carlosgsouza/nonograms/GridPrinter.kt
@@ -0,0 +1,47 @@
+package carlosgsouza.nonograms.carlosgsouza.nonograms
+
+import carlosgsouza.nonograms.Grid
+import carlosgsouza.nonograms.Line
+import carlosgsouza.nonograms.carlosgsouza.nonograms.Cell.BLANK
+import carlosgsouza.nonograms.carlosgsouza.nonograms.Cell.FILLED
+
+class GridPrinter {
+    fun toString(grid: Grid) : String {
+        val maxRowHints = grid.rowHints.maxOf { it.size }
+        val maxColumnHints = grid.columnHints.maxOf { it.size }
+
+        val rows = grid.rows.joinToString("\n") { rowToString(it, maxRowHints) }
+
+        // Each hint takes 2 characters, except for the last one, which takes just 1.
+        // The separator has 3 characters.
+        val columnHintsIndentation = " ".repeat(maxRowHints * 2 - 1 + 3)
+        val columnHints = grid.columns.map { c -> lineToStrings(maxColumnHints, c) }
+        val columnHintsString = (0 until maxColumnHints).asSequence()
+            .joinToString("\n") { hintIndex -> columnHintsIndentation + columnHints.joinToString(" ") { it[hintIndex] } }
+
+        val horizontalBorderIndentation = " ".repeat(2 * maxRowHints)
+        val horizontalBorderLine = "─".repeat(grid.size * 2)
+        val horizontalTopBorder = "$horizontalBorderIndentation┌─$horizontalBorderLine┐"
+        val horizontalBottomBorder = "$horizontalBorderIndentation└─$horizontalBorderLine┘"
+
+        return listOf(columnHintsString, horizontalTopBorder, rows, horizontalBottomBorder).joinToString("\n")
+    }
+
+    fun rowToString(row: Line, maxRowHints: Int): String {
+        val hints: List<String> = lineToStrings(maxRowHints, row)
+        val cells: List<String> = row.map { cellToString(it) }
+        return hints.joinToString(" ") + " │ " + cells.joinToString(" ") + " │"
+    }
+
+    private fun lineToStrings(totalSize: Int, line: Line) =
+        List(totalSize - line.hints.size) { " " } + line.hints.map { it.toString() }
+
+    fun cellToString(cell: Cell): String {
+        return when (cell) {
+            BLANK -> " "
+            FILLED -> "■"
+            else -> "?"
+        }
+    }
+}
+
