46dd0a0d93ba1a1e5eb95de87678567f455e1172
carlosgsouza <carlosgsouza@gmail.com>
2021-12-08 22:11:43 -0800

    Adding a builder for Grids

--- src/main/kotlin/carlosgsouza/nonograms/Grid.kt
+++ src/main/kotlin/carlosgsouza/nonograms/Grid.kt
@@ -14,4 +14,67 @@ class Grid(
      */
     val isSolved: Boolean
         get() = rows.all { it.isSolved } && columns.all { it.isSolved }
+
+    companion object {
+        /**
+         * Builds a new Grid. Example:
+         *
+         * val grid = Grid.new {
+         *   row(1, 1)
+         *   row(2)
+         *   row(1)
+         *
+         *   column(1, 1)
+         *   column(1)
+         *   column(2)
+         *
+         *   values(Cell.BLANK, Cell.BLANK, Cell.FILLED)
+         *   values(Cell.BLANK, Cell.UNKNOWN, Cell.FILLED)
+         *   values(Cell.BLANK, Cell.BLANK, Cell.FILLED)
+         * }
+         */
+        fun new(buildFun: GridBuilder.() -> Unit): Grid {
+            return GridBuilder.new(buildFun)
+        }
+    }
+}
+
+class GridBuilder {
+    val rowHints: MutableList<List<Int>> = mutableListOf()
+    val columnHints: MutableList<List<Int>> = mutableListOf()
+    val valueRows: MutableList<MutableList<Cell>> = mutableListOf()
+
+    companion object {
+        internal fun new(buildFun: GridBuilder.() -> Unit): Grid {
+            return GridBuilder().apply(buildFun).build()
+        }
+    }
+
+    fun row(vararg hints: Int) {
+        rowHints.add(hints.asList())
+    }
+
+    fun column(vararg hints: Int) {
+        columnHints.add(hints.asList())
+    }
+
+    fun values(vararg values: Cell) {
+        valueRows.add(values.asList().toMutableList())
+    }
+
+    private fun build(): Grid {
+        if(rowHints.size != columnHints.size) throw IllegalArgumentException("Number of rows and columns is different")
+        val size = rowHints.size
+
+        if(valueRows.isEmpty()) {
+            return Grid(size, rowHints, columnHints)
+        }
+        else {
+            if(valueRows.size != size) throw IllegalArgumentException("Number of values rows is different")
+            valueRows.forEach {
+                if(it.size != size) throw IllegalArgumentException("Value row does not have the right number of values")
+            }
+            return Grid(size, rowHints, columnHints, valueRows)
+        }
+    }
 }
--- src/test/kotlin/carlosgsouza/nonograms/GridTest.kt
+++ src/test/kotlin/carlosgsouza/nonograms/GridTest.kt
@@ -1,5 +1,8 @@
 package carlosgsouza.nonograms
 
+import carlosgsouza.nonograms.Cell.*
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
@@ -49,5 +52,36 @@ x _ ?
 """)
 
         assertFalse(unsolvedGrid.isSolved)
+    }
+
+    @Test
+    fun testNew() {
+        val grid = Grid.new {
+            row(1, 1)
+            row(2)
+            row(1)
+
+            column(1, 1)
+            column(1)
+            column(2)
+
+            values(FILLED, FILLED, BLANK)
+            values(BLANK, FILLED, UNKNOWN)
+            values(FILLED, BLANK, BLANK)
+        }
+
+        assertThat(grid.size, equalTo(3))
+
+        assertThat(grid.rowHints, equalTo(listOf(listOf(1, 1), listOf(2), listOf(1))))
+        assertThat(grid.columnHints, equalTo(listOf(listOf(1, 1), listOf(1), listOf(2))))
+
+        assertThat(grid.rows.size, equalTo(3))
+        assertThat(grid.columns.size, equalTo(3))
+
+        assertThat(grid.cells, equalTo(listOf(
+            listOf(FILLED, FILLED, BLANK),
+            listOf(BLANK, FILLED, UNKNOWN),
+            listOf(FILLED, BLANK, BLANK)
+        )))
     }
 }
